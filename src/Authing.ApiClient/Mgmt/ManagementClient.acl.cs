219050049cd43b223cd94986cc4d6c620d0680a2
shubuzuo <lixintao2@authing.cn>
2021-06-07 11:37:06 +0800

    add DeleteApplicationAccessPolicy DisableApplicationAccessPolicy

--- src/Authing.ApiClient/Mgmt/ManagementClient.acl.cs
+++ src/Authing.ApiClient/Mgmt/ManagementClient.acl.cs
@@ -261,7 +261,7 @@ namespace Authing.ApiClient.Mgmt
                 return res;
             }
 
-            public async Task<object> EnableApplicationAccessPolicy(AppAccessPolicy appAccessPolicy, CancellationToken cancellationToken = default)
+            public async Task<CommonMessage> EnableApplicationAccessPolicy(AppAccessPolicy appAccessPolicy, CancellationToken cancellationToken = default)
             {
                 if (appAccessPolicy.AppId == null)
                 {
@@ -285,7 +285,56 @@ namespace Authing.ApiClient.Mgmt
                 };
             }
 
+            public async Task<CommonMessage> DisableApplicationAccessPolicy(AppAccessPolicy appAccessPolicy, CancellationToken cancellationToken = default)
+            {
+                if (appAccessPolicy.AppId == null)
+                {
+                    throw new Exception("请传入 appId");
+                }
+                if (appAccessPolicy.TargetType == default)
+                {
+                    throw new Exception("请传入主体类型，可选值为 USER、ROLE、ORG、GROUP，含义为用户、角色、组织机构节点、用户分组");
+                }
+                if (appAccessPolicy.TartgetIdentifiers?.Length < 1)
+                {
+                    throw new Exception("请传入主体 id");
+                }
 
+                var res = await client.Host.AppendPathSegment($"api/v2/applications/{appAccessPolicy.AppId}/authorization/disable-effect").WithOAuthBearerToken(client.Token).PostJsonAsync(appAccessPolicy, cancellationToken);
+
+                return new CommonMessage
+                {
+                    Code = 200,
+                    Message = "停用应用访问控制策略成功"
+                };
+            }
+
+            public async Task<CommonMessage> DeleteApplicationAccessPolicy(AppAccessPolicy appAccessPolicy, CancellationToken cancellationToken = default
+            )
+            {
+                if (appAccessPolicy.AppId == null)
+                {
+                    throw new Exception("请传入 appId");
+                }
+                if (appAccessPolicy.TargetType == default)
+                {
+                    throw new Exception("请传入主体类型，可选值为 USER、ROLE、ORG、GROUP，含义为用户、角色、组织机构节点、用户分组");
+                }
+                if (appAccessPolicy.TartgetIdentifiers?.Length < 1)
+                {
+                    throw new Exception("请传入主体 id");
+                }
+
+                var res = await client.Host.AppendPathSegment($"api/v2/applications/{appAccessPolicy.AppId}/authorization/revoke").WithOAuthBearerToken(client.Token).PostJsonAsync(appAccessPolicy, cancellationToken);
+
+                return new CommonMessage
+                {
+                    Code = 200,
+                    Message = "删除应用访问控制策略成功"
+                };
+            }
+
+            
         }
     }
 }
